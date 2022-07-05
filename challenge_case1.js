7c67b4ce5d1ce5b53f08e6e60e4cc7b26c681e6a
yosuaindra <yosua.is.dev@gmail.com>
2022-07-05 22:20:19 +0700

    update js case 1

--- challenge_case1.js
+++ challenge_case1.js
@@ -4,5 +4,5 @@ var angka2 = prompt('input angka 2');
 var pengkuadratan = Math.pow(angka1, angka2);
 var pembagian = angka1/angka2;
 
-alert("hasil pengkuadratan angka "+angka1+" dan angka "+angka2+" = "+pengkuadratan);
-alert("hasil pembagian "+angka1+" dan "+angka2+" = "+pembagian);
+alert(`hasil pengkuadratan angka ${angka1} dan angka  ${angka2} =  ${pengkuadratan}`);
+alert(`hasil pembagian ${angka1} dan ${angka2} = ${pembagian}`);
